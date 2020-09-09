5f2f4ef067bb61a037672e09a10afa71dd936915
odedsalton <39484614+odedsalton@users.noreply.github.com>
2020-09-09 19:17:38 +0300

    Add files via upload

--- project_read_data.py
+++ project_read_data.py
@@ -8,17 +8,17 @@ def main():
     songs = ['song10','song9','song8','song7','song6','song5','song4','song3','song2','song1']
     df_greedy = pf.creating_user_dataframe(songs)
     df_our = pf.creating_user_dataframe(songs)
-    for i in range(20):
-        M = songs_data.sample(n=100)
-        M.reset_index()
-        z = M.sample(n=10)
-        n_bins = 10
-        songs_dict2 = gr.dj_mc_framework(M,10,z,z,10,10,50)
-        print("songs dict", songs_dict2)
-        df_greedy = pf.appending_series(df_greedy,songs_dict2,i)
-        songs_dict = al.dj_mc_framework(M,10,z,z,10,10,50)
-        df_our = pf.appending_series(df_our,songs_dict,i)
-        print("songs dict", songs_dict)
+
+    M = songs_data.sample(n=100)
+    M.reset_index()
+    z = M.sample(n=10)
+    n_bins = 10
+    songs_dict2 = gr.dj_mc_framework(M,10,z,z,10,10,50)
+    print("songs dict", songs_dict2)
+    df_greedy = pf.appending_series(df_greedy,songs_dict2,0)
+    songs_dict = al.dj_mc_framework(M,10,z,z,10,10,50)
+    df_our = pf.appending_series(df_our,songs_dict,0)
+    print("songs dict", songs_dict)
     df_greedy.to_csv('greedy_data.csv')
     df_our.to_csv('DJMC_data.csv')
 
